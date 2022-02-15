6bbe2bd7185c974fb49ae97883e3b9f347a00a52
punchitox <fjgarciabasanta@gmail.com>
2022-02-15 15:25:13 +0100

    ultimo import prueba

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,12 +11,12 @@ INSERT INTO departamentos(nombre,ubicacion) VALUES ('Mayordomia','Madrid');
 INSERT INTO departamentos(nombre,ubicacion) VALUES ('Recursos Humanos','Barcelona');
 INSERT INTO departamentos(nombre,ubicacion) VALUES ('Seguridad','Asturias');
 
-INSERT INTO empleados(dni,nombre,salario,telefono,departamento_id) VALUES ('Javi','50147852L',18000,654789547,5);
-INSERT INTO empleados(dni,nombre,salario,telefono,departamento_id) VALUES ('Jose','4456565T',22000,55466327,3);
-INSERT INTO empleados(dni,nombre,salario,telefono,departamento_id) VALUES ('Juan','54546565O',31000,4566245,4);
-INSERT INTO empleados(dni,nombre,salario,telefono,departamento_id) VALUES ('Jorge','45465R',25000,5565445,1);
-INSERT INTO empleados(dni,nombre,salario,telefono,departamento_id) VALUES ('Sandra','455459659F',4556,44664848,2);
-INSERT INTO empleados(dni,nombre,salario,telefono,departamento_id) VALUES ('Lidia','564646G',93330,665654,5);
+INSERT INTO empleados(nombre,dni,salario,telefono,departamento_id) VALUES ('Javi','50147852L',18000,654789547,5);
+INSERT INTO empleados(nombre,dni,salario,telefono,departamento_id) VALUES ('Jose','4456565T',22000,55466327,3);
+INSERT INTO empleados(nombre,dni,salario,telefono,departamento_id) VALUES ('Juan','54546565O',31000,4566245,4);
+INSERT INTO empleados(nombre,dni,salario,telefono,departamento_id) VALUES ('Jorge','45465R',25000,5565445,1);
+INSERT INTO empleados(nombre,dni,salario,telefono,departamento_id) VALUES ('Sandra','455459659F',4556,44664848,2);
+INSERT INTO empleados(nombre,dni,salario,telefono,departamento_id) VALUES ('Lidia','564646G',93330,665654,5);
 
-INSERT INTO jefes(dni,nombre,salario,telefono,departamento_id) VALUES ('Sara','54632479P',120000,66545448,5);
-INSERT INTO jefes(dni,nombre,salario,telefono,departamento_id) VALUES ('Noelia','5454515Y',45000,65455454,2);
+INSERT INTO jefes(nombre,dni,salario,telefono,departamento_id) VALUES ('Sara','54632479P',120000,66545448,5);
+INSERT INTO jefes(nombre,dni,salario,telefono,departamento_id) VALUES ('Noelia','5454515Y',45000,65455454,2);
